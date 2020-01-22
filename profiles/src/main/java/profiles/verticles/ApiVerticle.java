1430d428fc1c356294ee61a1cd0b3021ca2fd53a
B1Z0N <kolausf@gmail.com>
2020-01-23 00:45:43 +0200

    Added structure and logic to API verticle

--- profiles/src/main/java/profiles/verticles/ApiVerticle.java
+++ profiles/src/main/java/profiles/verticles/ApiVerticle.java
@@ -1,14 +1,11 @@
-package scales.verticles;
+package profiles.verticles;
 
 import io.vertx.kafka.client.common.TopicPartition;
 import io.vertx.kafka.client.consumer.KafkaConsumer;
 import io.vertx.kafka.client.producer.KafkaProducer;
 import io.vertx.kafka.client.producer.KafkaProducerRecord;
-import scales.model.Config;
-import scales.model.ConfigMessageCodec;
-import scales.model.OriginID;
-import scales.model.OriginID.photoType;
-import scales.model.OriginIDCodec;
+import profiles.model.Config;
+import profiles.model.ConfigMessageCodec;
 
 import vertx.common.MicroserviceVerticle;
 import io.vertx.core.Promise;
@@ -17,10 +14,8 @@ import io.vertx.core.json.JsonObject;
 import javax.annotation.Nonnull;
 import java.util.*;
 
-import static scales.verticles.ConfigurationVerticle.EBA_CONFIG_FETCH;
-import static scales.verticles.ConfigurationVerticle.EBA_CONFIG_UPDATE;
-import static scales.verticles.ScaleVerticle.EBA_DELETE_ORIGIN;
-import static scales.verticles.ScaleVerticle.EBA_SCALE_ORIGIN;
+import static profiles.verticles.ConfigurationVerticle.EBA_CONFIG_FETCH;
+import static profiles.verticles.ConfigurationVerticle.EBA_CONFIG_UPDATE;
 
 // verticle for communicating with kafka and internal implementation
 public class ApiVerticle extends MicroserviceVerticle {
@@ -28,16 +23,14 @@ public class ApiVerticle extends MicroserviceVerticle {
   // Overrides
 
   private KafkaConsumer<String, String> mConsumer;
-  private KafkaProducer<String, String> mSagasProducer;
+  private KafkaProducer<String, String> mProducer;
 
   private String mKafkaHost;
   private String mKafkaPort;
-  private String mScaleRequest;
-  private String mDeleteRequest;
-  private String mUserpicsTopic;
-  private String mPhotosTopic;
-  private String mSagasTopic;
-
+  private String mGeoSearchTopic;
+  private String mTagSearchTopic;
+  private String mGeoOutputTopic;
+  private String mTagOutputTopic;
 
   // Overrides
 
@@ -54,30 +47,30 @@ public class ApiVerticle extends MicroserviceVerticle {
   private void setupFromConfig(@Nonnull Config config) {
     mKafkaHost = config.getKafkaHost();
     mKafkaPort = config.getKafkaPort();
-    mUserpicsTopic = config.getUserpicsTopic();
-    mPhotosTopic = config.getPhotosTopic();
-    mDeleteRequest = config.getDeleteRequest();
-    mScaleRequest = config.getScaleRequest();
-    mSagasTopic = config.getSagasTopic();
+    mGeoSearchTopic = config.getGeoSearchTopic();
+    mTagSearchTopic = config.getTagSearchTopic();
+    mGeoOutputTopic = config.getGeoOutputTopic();
+    mTagOutputTopic = config.getTagOutputTopic();
 
     if (mConsumer != null) mConsumer.unsubscribe();
-    setupSagasProducer();
-    setupKafkaConsumers();
+
+    setupKafkaConsumer();
+    setupKafkaProducer();
   }
 
 
-  private void setupSagasProducer() {
+  private void setupKafkaProducer() {
     Map<String, String> kafkaConfig = new HashMap<>();
     kafkaConfig.put("bootstrap.servers", String.join(":", mKafkaHost, mKafkaPort));
     kafkaConfig.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
     kafkaConfig.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
     kafkaConfig.put("group.id", "my_group");
 
-    mSagasProducer = KafkaProducer.create(vertx, kafkaConfig);
+    mProducer = KafkaProducer.create(vertx, kafkaConfig);
   }
 
 
-  private void setupKafkaConsumers() {
+  private void setupKafkaConsumer() {
     Map<String, String> config = new HashMap<>();
     config.put("bootstrap.servers", String.join(":", mKafkaHost, mKafkaPort));
     config.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
@@ -89,73 +82,37 @@ public class ApiVerticle extends MicroserviceVerticle {
     mConsumer = KafkaConsumer.create(getVertx(), config);
     mConsumer.handler(record -> {
       vinfo("Handling record: " + record.topic() + "  " + ":" + record.value());
-
-      photoType type = record.topic().equals(mUserpicsTopic) ? photoType.USERPIC : photoType.PHOTO;
-
-      if (record.value().startsWith(mScaleRequest))
-        photoScale(record.value().substring(mScaleRequest.length()), type);
-      else if (record.value().startsWith(mDeleteRequest))
-        photoDelete(record.value().substring(mDeleteRequest.length()), type);
+      // 1. search request to elasticsearch
+      // 2. parsing it
+      // 3. output to outputTopic
+      if (record.topic().equals(mGeoSearchTopic)) {
+        // geo data is being searched
+      } else {
+        // tag data is being searched
+      }
     });
 
     Set<String> topics = new HashSet<>();
-    topics.add(mUserpicsTopic);
-    topics.add(mPhotosTopic);
+    topics.add(mGeoSearchTopic);
+    topics.add(mTagSearchTopic);
 
     mConsumer.subscribe(topics, ar -> {
       if (ar.succeeded()) {
-        vsuccess(String.format("Subscribed to %s and %s successfully", mUserpicsTopic, mPhotosTopic));
+        vsuccess(String.format("Subscribed to %s and %s successfully", mGeoSearchTopic, mTagSearchTopic));
       } else {
         verror(
-                String.format("Could not subscribe to %s or %s: ", mUserpicsTopic, mPhotosTopic) + ar.cause().getMessage()
+                String.format("Could not subscribe to %s or %s: ", mGeoSearchTopic, mTagSearchTopic) + ar.cause().getMessage()
         );
       }
     });
-
-    new TopicPartition(mUserpicsTopic, 0);
-    Set<TopicPartition> topicPs = new HashSet<>();
-    topicPs.add(new TopicPartition(mUserpicsTopic, 0));
-    topicPs.add(new TopicPartition(mPhotosTopic, 0));
   }
 
 
-  private void sagas(String msg) {
-    mSagasProducer.write(
-            KafkaProducerRecord.create(mSagasTopic, msg)
-    );
-  }
-
-
-  private void photoScale(@Nonnull String ID, photoType type) {
-    vertx.eventBus().<OriginID>request(EBA_SCALE_ORIGIN, new OriginID(ID, type), ar -> {
-      if (ar.failed()) {
-        // send "ERR" to sagas
-        verror("Scaling, " + type.toString() + " : " + ID + " | " + ar.cause());
-        sagas("photo-scale:" + mScaleRequest + "err:" + ID);
-        return;
-      }
-
-      // send "OK" to sagas
-      vsuccess("Scaling, " + type.toString() + " : " + ID);
-      sagas("photo-scale:" + mScaleRequest + "ok:" + ID);
-    });
-  }
-
-
-  private void photoDelete(@Nonnull String ID, photoType type) {
-    vertx.eventBus().<OriginID>request(EBA_DELETE_ORIGIN, new OriginID(ID, type), ar -> {
-      if (ar.failed()) {
-        // send "ERR" to sagas
-        verror("Deleting, " + type.toString() + " : " + ID + " | " + ar.cause());
-        sagas("photo-scale:" + mDeleteRequest + ":err:" + ID);
-        return;
-      }
-
-      // send "OK" to sagas
-      vsuccess("" + type.toString() + " : " + ID);
-      sagas("photo-scale:" + mDeleteRequest + "ok:" + ID);
-    });
-  }
+//  private void sagas(String msg) {
+//    mSagasProducer.write(
+//            KafkaProducerRecord.create(mSagasTopic, msg)
+//    );
+//  }
 
 
   /**
@@ -165,10 +122,6 @@ public class ApiVerticle extends MicroserviceVerticle {
   private void registerCodecs() {
     try {
       vertx.eventBus().registerDefaultCodec(Config.class, new ConfigMessageCodec());
-    } catch (IllegalStateException ignored) {
-    }
-    try {
-      vertx.eventBus().registerDefaultCodec(OriginID.class, new OriginIDCodec());
     } catch (IllegalStateException ignored) {
     }
   }
