3acc059139e3e5d3c9c6413ee7ec6f84de5d8cff
B1Z0N <kolausf@gmail.com>
2020-01-23 00:03:00 +0200

    Added logging to configuration verticle

--- profiles/src/main/java/profiles/verticles/ConfigurationVerticle.java
+++ profiles/src/main/java/profiles/verticles/ConfigurationVerticle.java
@@ -8,13 +8,15 @@ import io.vertx.config.ConfigRetriever;
 import io.vertx.core.Promise;
 import io.vertx.core.json.JsonObject;
 
-/** Verticle for configuration maintenance update/retrieval */
+/**
+ * Verticle for configuration maintenance update/retrieval
+ */
 public class ConfigurationVerticle extends MicroserviceVerticle {
 
   // Constants
 
-  static final String EBA_CONFIG_FETCH = "configuration:fetch";
-  static final String EBA_CONFIG_UPDATE = "configuration:update";
+  public static final String EBA_CONFIG_FETCH = "configuration:fetch";
+  public static final String EBA_CONFIG_UPDATE = "configuration:update";
 
   // Variables
 
@@ -24,7 +26,6 @@ public class ConfigurationVerticle extends MicroserviceVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) {
-    createServiceDiscovery();
     registerCodecs();
     setupRetriever(startPromise);
     setupListener();
@@ -35,31 +36,29 @@ public class ConfigurationVerticle extends MicroserviceVerticle {
   private void registerCodecs() {
     try {
       vertx.eventBus().registerDefaultCodec(Config.class, new ConfigMessageCodec());
-    } catch (IllegalStateException ignored) {}
+    } catch (IllegalStateException ignored) {
+    }
   }
 
   private void setupRetriever(Promise<Void> startPromise) {
     ConfigRetriever retriever = ConfigRetriever.create(vertx);
     retriever.getConfig(configAr -> {
       if (configAr.failed()) {
+        verror("Setup");
         startPromise.fail(configAr.cause());
         return;
       }
 
+      vsuccess("Setup");
       mConfig = new Config(configAr.result());
-
-      publishMessageSource(EBA_CONFIG_UPDATE, EBA_CONFIG_UPDATE, publishAr -> {
-        if (publishAr.failed()) {
-          startPromise.fail(publishAr.cause());
-        } else {
-          vertx.eventBus().publish(EBA_CONFIG_UPDATE, mConfig);
-
-          startPromise.complete();
-        }
-      });
+      startPromise.complete();
     });
+
     retriever.listen(this::onConfigChange);
-    retriever.configStream().exceptionHandler(e -> System.out.println("Error. Config file not found"));
+    retriever.configStream().exceptionHandler(e -> {
+      verror("Config file not found");
+      startPromise.fail("Config file not found");
+    });
   }
 
   private void onConfigChange(ConfigChange change) {
