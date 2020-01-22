eade6c82d59e67dec8b6ab702662b6a2b2ccb6ed
B1Z0N <kolausf@gmail.com>
2020-01-23 00:07:06 +0200

    Changed main verticle to use logging and right order of instantiation

--- profiles/src/main/java/profiles/verticles/MainVerticle.java
+++ profiles/src/main/java/profiles/verticles/MainVerticle.java
@@ -12,57 +12,60 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-/** Verticle that deploys all other verticles */
+/**
+ * Verticle that deploys all other verticles
+ */
 public class MainVerticle extends MicroserviceVerticle {
-
   // Variables
-
-  private Map<String, List<String>> mVerticles = new HashMap<>();
-
-  // Overrides
+  private Map<String, List<String>> mVerticles = new HashMap<>(); // Overrides
 
   @Override
   public void start(Promise<Void> startPromise) {
-    createServiceDiscovery();
-
     List<Promise> verticlePromises = Stream.of(
-        ConfigurationVerticle.class,
-        ProfilesVerticle.class,
-        ApiVerticle.class,
-        ServiceDiscoveryVerticle.class)
-      .map(el -> redeployVerticle(el.getName(), new JsonObject()))
-      .collect(Collectors.toList());
+            ConfigurationVerticle.class)
+            .map(el -> redeployVerticle(el.getName(), new JsonObject()))
+            .collect(Collectors.toList());
 
     List<Future> futures = verticlePromises.stream()
-      .map((Function<Promise, Future>) Promise::future)
-      .collect(Collectors.toList());
+            .map((Function<Promise, Future>) Promise::future)
+            .collect(Collectors.toList());
 
     CompositeFuture.all(futures).setHandler(ar -> {
       if (ar.failed()) {
         startPromise.fail(ar.cause());
-      } else {
-        startPromise.complete();
+        verror("Setup");
       }
+    }).compose(v -> {
+      // set up API verticle, when configuration verticle is up
+      return redeployVerticle(ApiVerticle.class.getName(), new JsonObject())
+              .future().setHandler(
+                      ar -> {
+                        if (ar.failed()) {
+                          startPromise.fail(ar.cause());
+                          verror("Setup");
+                        } else {
+                          startPromise.complete();
+                          vsuccess("Setup");
+                        }
+                      });
     });
   }
 
   // Private
-
   private Promise<Void> redeployVerticle(String className, JsonObject config) {
     Promise<Void> completion = Promise.promise();
     removeExistingVerticles(className);
-
-    DeploymentOptions options = new DeploymentOptions()
-      .setConfig(config);
+    DeploymentOptions options = new DeploymentOptions().setConfig(config);
     vertx.deployVerticle(className, options, ar -> {
       if (ar.failed()) {
         completion.fail(ar.cause());
+        verror("Deploy: " + className + ", reason: " + ar.cause());
       } else {
         registerVerticle(className, ar.result());
         completion.complete();
+        vsuccess("Deploy: " + className);
       }
     });
-
     return completion;
   }
 
@@ -73,8 +76,7 @@ public class MainVerticle extends MicroserviceVerticle {
   }
 
   private void removeExistingVerticles(String className) {
-    mVerticles.getOrDefault(className, new ArrayList<>())
-      .forEach(vertx::undeploy);
+    mVerticles.getOrDefault(className, new ArrayList<>()).forEach(vertx::undeploy);
     mVerticles.remove(className);
   }
 }
